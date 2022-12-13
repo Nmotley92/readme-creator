3539028a6e61ed3094109556e2c149e41529493f
Nathaniel Motley <nmotley92@gmail.com>
2022-12-12 21:34:49 -0600

    adds all the questions for the prompts

---
+++ README.md
@@ -0,0 +1,44 @@
+# Readme Creator
+
+  [![GitHub license](https://img.shields.io/badge/License-Eclipse_2.0-blue.svg)](https://www.eclipse.org/legal/epl-2.0/)
+
+  ## Description 
+  Quickly generates readmes
+
+  ## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributions](#contributions)
+- [Tests](#tests)
+- [License](#license)
+
+
+## Installation
+npm install
+
+## Usage 
+n/a
+
+## Contributions
+
+n/a
+  
+## Tests
+
+n/a
+
+##License
+
+  This product is licensed under- <a href="https://www.eclipse.org/legal/epl-2.0/">Eclipse_2.0</a>
+
+## Questions 
+
+If you have any questions please contact me
+
+
+Github: [nmotley92](https://github.com/nmotley92)
+
+
+Email: nmotley92@gmail.com
+
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
+// questions for the users input
 const questions = [
     {
         type: 'input',
@@ -17,17 +17,110 @@ const questions = [
         },
 
     },
-    
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is a description of your project?',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter a description");
+            return false;
+        },
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install your project?',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter how to install your project");
+            return false;
+        },
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage of project?',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter the usage");
+            return false;
+        },
+
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute?.',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter how to contribute");
+            return false;
+        },
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What commands can be run for tests?.',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter the tests");
+            return false;
+        },
+    },
+
+    {
+        type: 'list',
+        choices: ['Apache_2.0', 'MIT', 'Boost_1.0', 'Perl', 'Eclipse_2.0',
+            'GNU_2.0', 'Mozilla_Public', 'NONE'
+        ],
+        name: 'license',
+        message: 'Please Choose a License.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username?',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter your gitHub username");
+            return false;
+        },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email please.',
+        validate: (Answer) => {
+            if (Answer) {
+                return true;
+            } else
+                console.log("Please enter your email");
+            return false;
+        },
+    },
 
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateMarkdown(data), (err) =>
         err ? console.error(err) : console.log('Created README.md!')
     )
 }
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions).then((response) => {
         writeToFile("README.md", response);
